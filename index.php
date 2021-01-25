51a43b07ca748742cc333f5db11d881a031cf757
Kim John A. Hidalgo <kimpoyhidalgo@gmail.com>
2021-01-25 10:38:08 +0800

    test

--- index.php
+++ index.php
@@ -15,94 +15,98 @@ $dqa = new \app\Dqa();
 
 session_start();
 require 'vendor/autoload.php';
-$provider = new \Stevenmaguire\OAuth2\Client\Provider\Keycloak([
-    'authServerUrl' => 'http://auth.caraga.dswd.gov.ph:8080/auth',
-    'realm' => 'entdswd.local',
-    'clientId' => 'kalahi-apps',
-    'clientSecret' => '07788f27-8e6a-4729-a033-0eb5cb7c7389',
-    'redirectUri' => 'http://crg-kcapps-svr/mrms/index.php'
-]);
 
-if (!isset($_GET['code'])) {
+if(!$_SESSION['login']){
+    $provider = new \Stevenmaguire\OAuth2\Client\Provider\Keycloak([
+        'authServerUrl' => 'http://auth.caraga.dswd.gov.ph:8080/auth',
+        'realm' => 'entdswd.local',
+        'clientId' => 'kalahi-apps',
+        'clientSecret' => '07788f27-8e6a-4729-a033-0eb5cb7c7389',
+        'redirectUri' => 'http://crg-kcapps-svr/mrms/index.php'
+    ]);
 
-    // If we don't have an authorization code then get one
-    $authUrl = $provider->getAuthorizationUrl();
-    $_SESSION['oauth2state'] = $provider->getState();
+    if (!isset($_GET['code'])) {
 
-    //var_dump($authUrl);
-    header('Location: '.$authUrl);
-    exit;
+        // If we don't have an authorization code then get one
+        $authUrl = $provider->getAuthorizationUrl();
+        $_SESSION['oauth2state'] = $provider->getState();
+
+        //var_dump($authUrl);
+        header('Location: '.$authUrl);
+        exit;
 
 // Check given state against previously stored one to mitigate CSRF attack
-} elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
-    session_start();
-    echo $_SESSION['oauth2state'] . " get oauth<br>";
-    echo $_SESSION['state'] . " get state: <br>";
-    unset($_SESSION['oauth2state']);
-    exit('Invalid state, make sure HTTP sessions are enabled.');
+    } elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
+        session_start();
+        echo $_SESSION['oauth2state'] . " get oauth<br>";
+        echo $_SESSION['state'] . " get state: <br>";
+        unset($_SESSION['oauth2state']);
+        exit('Invalid state, make sure HTTP sessions are enabled.');
 
-} else {
+    } else {
 
 
 
-    // Try to get an access token (using the authorization coe grant)
-    try {
-        $token = $provider->getAccessToken('authorization_code', [
-            'code' => $_GET['code']
-        ]);
-    } catch (Exception $e) {
-        exit('Failed to get access token: ' . $e->getMessage());
-    }
+        // Try to get an access token (using the authorization coe grant)
+        try {
+            $token = $provider->getAccessToken('authorization_code', [
+                'code' => $_GET['code']
+            ]);
+        } catch (Exception $e) {
+            exit('Failed to get access token: ' . $e->getMessage());
+        }
 
-    // Optional: Now you have a token you can look up a users profile data
-    try {
+        // Optional: Now you have a token you can look up a users profile data
+        try {
 
-        // We got an access token, let's now get the user's details
-        $user_sso = $provider->getResourceOwner($token);
+            // We got an access token, let's now get the user's details
+            $user_sso = $provider->getResourceOwner($token);
 
-        //var_dump($user->sso_isExist($user_sso));
-        //die();
+            //var_dump($user->sso_isExist($user_sso));
+            //die();
 
-        if ($user->sso_isExist($user_sso)) {
-            //$auth->redirectTo('../index.php');
-            //KUHAIN NYA ANG USERNAME UG PASSWORD BASED SA OAUATH PARAM
-            $user_sso = $user_sso->toArray();
-            $oauth = $user_sso['sub'];
+            if ($user->sso_isExist($user_sso)) {
+                //$auth->redirectTo('../index.php');
+                //KUHAIN NYA ANG USERNAME UG PASSWORD BASED SA OAUATH PARAM
+                $user_sso = $user_sso->toArray();
+                $oauth = $user_sso['sub'];
 
-            $q = "SELECT
+                $q = "SELECT
             users.username,
             users.password
             FROM
             users where oauth_client='$oauth'";
-            $result = $mysql->query($q);
-            $row = $result->fetch_assoc();
+                $result = $mysql->query($q);
+                $row = $result->fetch_assoc();
 
-            $username = $row['username'];
-            $password = $row['password'];
-            $app->login($username,$password);
+                $username = $row['username'];
+                $password = $row['password'];
+                $app->login($username,$password);
 
-            //IPASA SA LOGIN NGA FUNCTION FOR  SESSION
-        } else {
-            $user->register_sso($user_sso);
+                //IPASA SA LOGIN NGA FUNCTION FOR  SESSION
+            } else {
+                $user->register_sso($user_sso);
 
+            }
+            header('location: index.php');
+
+            //1. check nya ang naka session database
+
+            //2. pag walay user unya oauth wala nag exist, e create nya
+
+            //3. go to urlshit
+
+            // Use these details to create a new profile
+            printf('Hello %s!', $user->getName());
+
+        } catch (Exception $e) {
+            exit('Failed to get resource owner: ' . $e->getMessage());
         }
-        header('location: index.php');
 
-        //1. check nya ang naka session database
-
-        //2. pag walay user unya oauth wala nag exist, e create nya
-
-        //3. go to urlshit
-
-        // Use these details to create a new profile
-        printf('Hello %s!', $user->getName());
-
-    } catch (Exception $e) {
-        exit('Failed to get resource owner: ' . $e->getMessage());
+        // Use this to interact with an API on the users behalf
+        echo $token->getToken();
     }
 
-    // Use this to interact with an API on the users behalf
-    echo $token->getToken();
 }
 
 
