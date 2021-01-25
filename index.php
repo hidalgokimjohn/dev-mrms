ba5817efe15ee1d545db3aa5ac18ada4cadc9287
Kim John A. Hidalgo <kimpoyhidalgo@gmail.com>
2021-01-25 09:39:59 +0800

    test

--- index.php
+++ index.php
@@ -14,8 +14,7 @@ if (!isset($_GET['code'])) {
     // If we don't have an authorization code then get one
     $authUrl = $provider->getAuthorizationUrl();
     $_SESSION['oauth2state'] = $provider->getState();
-    echo $_SESSION['oauth2state'] . " get oauth<br>";
-    echo $_SESSION['state'] . " get oauth<br>";
+
     //var_dump($authUrl);
     header('Location: '.$authUrl);
     exit;
@@ -23,10 +22,11 @@ if (!isset($_GET['code'])) {
 // Check given state against previously stored one to mitigate CSRF attack
 } elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
     session_start();
-    echo $_SESSION['oauth2state'] . " get oauth";
+    echo $_SESSION['oauth2state'] . " get oauth<br>";
+    echo $_SESSION['state'] . " get state: <br>";
     unset($_SESSION['oauth2state']);
     exit('Invalid state, make sure HTTP sessions are enabled.');
-    die();
+
 } else {
 
     // Try to get an access token (using the authorization coe grant)
@@ -48,6 +48,7 @@ if (!isset($_GET['code'])) {
             //$auth->redirectTo('../index.php');
         } else {
             $user->register_sso($user_sso);
+
         }
 
         //1. check nya ang naka session database
