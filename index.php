7d7847b2d9367cb30a94bf8deb0393d52458f89e
Kim John A. Hidalgo <kimpoyhidalgo@gmail.com>
2021-01-27 12:12:30 +0800

    test

--- index.php
+++ index.php
@@ -56,13 +56,13 @@ if(!$_SESSION['mrms_auth']){
                 $user_sso = $user_sso->toArray();
                 $oauth = $user_sso['sub'];
                 $_SESSION['mrms_auth'] = $oauth;
-               // $app->login_sso($user_sso['preferred_username']);
+                $app->login_sso($user_sso['preferred_username']);
             } else {
                 $user->register_sso($user_sso);
                 $user_sso = $user_sso->toArray();
                 $oauth = $user_sso['sub'];
                 $_SESSION['mrms_auth'] = $oauth;
-                //$app->login_sso($user_sso['preferred_username']);
+                $app->login_sso($user_sso['preferred_username']);
             }
 
         } catch (Exception $e) {
