544da60dcd26cac779a14de86ff1200a1be2593e
Kim John A. Hidalgo <kimpoyhidalgo@gmail.com>
2021-02-11 11:21:44 +0800

    added allow access origin

--- api/get_category.php
+++ api/get_category.php
@@ -3,6 +3,6 @@ include_once("../app/Database.php");
 include_once("../app/App.php");;
 include_once("../app/Ceac.php");
 header('Content-type:application/json;charset=utf-8');
-
+header('Access-Control-Allow-Origin:*');
 $api = new \app\App();
 $api->apiCategory('ipcdd_drom');
