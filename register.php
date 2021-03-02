22fff53cfe64a21a691807f2c302a664faad8518
Kim John A. Hidalgo <kimpoyhidalgo@gmail.com>
2021-03-02 15:41:43 +0800

    t
    t

--- register.php
+++ register.php
@@ -121,7 +121,7 @@ if ($_SESSION['forIDNumber']!=='true') {
                 </div>';
                         }else{
                             //it should be username in the first parameter
-                            var_dump($_SESSION['user_sso']);
+                            //var_dump($_SESSION['user_sso']);
                             $app->register_sso($_SESSION['user_sso'],$_SESSION['id_number']);
                             /*if($r){
                                 header('location: home.php');
