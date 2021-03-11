353b2297f0fcad3082ce8abc4eb6e99112ce7308
Kim John A. Hidalgo <kimpoyhidalgo@gmail.com>
2021-03-11 09:21:13 +0800

    added chatlio

--- home.php
+++ home.php
@@ -207,6 +207,14 @@ if (!$auth->loggedIn()) {
     </div>
 </div>
 </body>
+<script type="text/javascript">
+    window._chatlio = window._chatlio||[];
+    !function(){ var t=document.getElementById("chatlio-widget-embed");if(t&&window.ChatlioReact&&_chatlio.init)return void _chatlio.init(t,ChatlioReact);for(var e=function(t){return function(){_chatlio.push([t].concat(arguments)) }},i=["configure","identify","track","show","hide","isShown","isOnline", "page", "open", "showOrHide"],a=0;a<i.length;a++)_chatlio[i[a]]||(_chatlio[i[a]]=e(i[a]));var n=document.createElement("script"),c=document.getElementsByTagName("script")[0];n.id="chatlio-widget-embed",n.src="https://w.chatlio.com/w.chatlio-widget.js",n.async=!0,n.setAttribute("data-embed-version","2.3");
+        n.setAttribute('data-widget-id','f599c9b6-02ea-454f-5e9a-bf08f03c93c2');
+        c.parentNode.insertBefore(n,c);
+    }();
+</script>
+
 
 <script src="resources/js/jquery-3.5.1.js"></script>
 <script src="resources/js/app.js"></script>
